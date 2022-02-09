3e0f616162a93037a54d5fb2444c129b6e8a739f
xavireina <xavierreina93@gmail.com>
2022-02-09 20:21:22 +0100

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import contacts from "./contacts.json";
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+  const [firstNContacts, setContacts] = useState(contacts.slice(0, 5))
+
+  const randomContact = () => {
+    const newContact = contacts[Math.floor(Math.random()*contacts.length -1)]
+    const isIncluded = firstNContacts.find(contact => contact.name === newContact.name)
+    if (!isIncluded) setContacts([newContact, ...firstNContacts])
+    else randomContact();
+  }
+
+  const sortPopularity = () => {
+    setContacts(contacts => {
+      const sortedPopularity = contacts.sort((a,b) => b.popularity - a.popularity)
+      return [... sortedPopularity]
+    })
+  }
+
+  const sortName = () => {
+    setContacts(contacts => {
+      const sortedNames = contacts.sort((a, b) => a.name.toUpperCase().localeCompare(b.name.toUpperCase()))
+      return [...sortedNames]
+    })
+  }
+
+  const deleteContact = (id) => {
+    const filteredContacts = firstNContacts.filter((contact) => {
+      return contact.id !== id
+    })
+    setContacts(filteredContacts)
+  }
+  return <div className="App">
+
+<h1>IronContacts</h1>
+
+<button onClick={randomContact}> Add Random Contact</button>
+<button onClick={sortPopularity}>Sort by popularity</button>
+<button onClick={sortName}>Sort by name</button>
+
+<table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Won Oscar</th>
+            <th>Won Emmy</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+       {firstNContacts.map(contact => {
+
+       return (
+        <tbody>
+          <td>
+            <img src={contact.pictureUrl} alt='' height='150' />
+          </td>
+          <td>{contact.name}</td>
+          <td>{contact.popularity.toFixed(2)}</td>
+          <td>{contact.wonOscar ?  <p>🏆</p> : <p></p> }</td>
+          <td>{contact.wonEmmy ?  <p>🏆</p> : <p></p> }</td>
+          <td> <button onClick={() => deleteContact(contact.id)}> Delete </button></td>
+        </tbody>
+
+       )})}
+      </table>
+  </div>;
+}
 export default App;
